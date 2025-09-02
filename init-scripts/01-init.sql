09a7b6675d27986f043dd07d420dd5350f714222
Catch@123Lara <abhisheks2010>
2025-09-02 11:00:19 +0530

    SouthDubai changes

--- init-scripts/01-init.sql
+++ init-scripts/01-init.sql
@@ -6,10 +6,14 @@ CREATE DATABASE IF NOT EXISTS `allcdr_shams`;
 -- Create the database for the 'spc' customer
 CREATE DATABASE IF NOT EXISTS `allcdr_spc`;
 
+-- Create the database for the 'dubaiSouth' customer
+CREATE DATABASE IF NOT EXISTS `allcdr_dubaisouth`;
+
 -- Grant all necessary privileges to the user defined in the .env file
 -- This allows the user to connect to and manage both customer databases.
 GRANT ALL PRIVILEGES ON `allcdr_shams`.* TO '${DB_USER}'@'%';
 GRANT ALL PRIVILEGES ON `allcdr_spc`.* TO '${DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON `allcdr_dubaisouth`.* TO '${DB_USER}'@'%';
 
 -- Apply the new privileges
 FLUSH PRIVILEGES;
